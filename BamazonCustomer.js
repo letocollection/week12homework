c9b17315b0c5715c577984bbf116f3d137fdb0ca
letocollection <leonard@letocollection.com>
2016-09-21 15:30:08 -0700

    added nexttrans function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,8 @@
  var prompt = require('prompt');
 
- var Prompt = require('prompt-improved');
+ // var Prompt = require('prompt-improved');
+
+ var inquirer = require('inquirer')
 
 
  var mysql = require('mysql');
@@ -27,40 +29,92 @@
          console.log(res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].Price);
      }
      console.log("-----------------------------------");
+
+     start();
  })
 
- var prompt = new prompt();
+ var start = function() {
+     inquirer.prompt([{
+         name: "itemChoice",
+         type: "input",
+         message: "Which Item would you like to buy?",
 
- // prompt.ask('What is the Item ID of the item you would like to buy?', function(err, res) {
- //     if (err) return console.error(err);
- //     console.log('Response: ' + res);
+     }, {
+         name: "quantity",
+         type: "input",
+         message: "How many would you like to buy?"
+     }]).then(function(answer) {
+
+         console.log(answer.itemChoice);
+         console.log(answer.quantity);
+         connection.query('SELECT ItemID, ProductName, StockQuantity FROM products WHERE ?', { ItemID: answer.itemChoice }, function(err, res) {
+             console.log("Position: " + res[0].ItemID + " || Product: " + res[0].ProductName + " || Quantity: " + res[0].StockQuantity);
+
+             if (answer.quantity < res[0].StockQuantity) {
+                 console.log("You have successfuly purchased " + answer.quantity + " of the " + res[0].ProductName);
+                 nexttrans();
+             } else {
+                 console.log("Insufficient Quantity!")
+                 nexttrans();
+             }
+
+         })
+
+     })
+ };
+
+
+ var nexttrans = function() {
+     inquirer.prompt({
+         name: "continue",
+         type: "rawlist",
+         message: "Would you like to purchase something else? [YES][NO]",
+         choices: ["YES", "NO"]
+
+     }).then(function(user) {
+         if (user.continue.toUpperCase() == "YES") {
+             start()
+         } else {
+             console.log("Thankyou for shopping with Bamazon!");
+             process.exit();
+         }
+     })
+ };
+
+
+
+
+ // prompt.start();
+
+ // var Question1 = 'What is the Item ID of the Item you would like to buy?';
+ // var Question2 = 'How many would you like to buy?';
+
+ // prompt.get([Question1, Question2], function(err, result) {
+
+ //     var Question1 = 'What is the Item ID of the Item you would like to buy?';
+ //     var Question2 = 'How many would you like to buy?';
+
+ //     console.log(Question1.result);
+ //     console.log(Question2.result);
+
  // });
 
- // prompt.ask('How many would you like to buy', function(err, res) {
- // 	if (err) return console.err(err);
- // 	console.log('Resonse: ' + res);
+ // prompt.get([{
 
- // });
-
-//  prompt.ask([{
-//     question: 'What is the Item ID of the item you would like to buy?',
-//     key: 'itemID',
-// }, {
-//     question: 'How many would you like to buy?',
-// }], function(err, res) {
-//     if (err) return console.error(err);
-//     console.log('Item Chosen: ' res.ItemID);
-//     console.log('Quantity: ' res['How many would you like to buy?']);
-// });
-
-var prompt = new Prompt();
-prompt.ask([{
-    question: 'Who\'s your daddy?',
-    key: 'father',
-}, {
-    question: 'What is your Mothers name?',
-}], function(err, res) {
-    if (err) return console.error(err);
-    console.log('Father\'s name: ' res.father);
-    console.log('Mother\'s name: ' res['What is your Mothers name?']);
-});
+ //       name: 'What is the item ID # that you would like to buy?',
+ //       required: true
+ //     }, {
+ //       name: 'How many would you like to buy?',
+ //       hidden: true,
+ //       conform: function (value) {
+ //         return true;
+ //       }
+ //     }], function (err, result) {
+ //     // 
+ //     // Log the results. 
+ //     // 
+ //     console.log('Command-line input received:');
+ //     console.log('  username: ' + result.username);
+ //     console.log('  password: ' + result.password);
+ //   });
+ //
